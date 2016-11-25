6a2b36b6bdc4b64edd876375897a374981746df4
Ibrahim Abdulkadir <lethalskillzz@gmail.com>
2016-11-25 10:04:19 +0100

    Update GeoIP.c

--- GeoIP.c
+++ GeoIP.c
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+   
 #include "GeoIP.h"
    
 static geoipv6_t IPV6_NULL;
